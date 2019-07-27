6ee4b71eda089e6d94e51303e5c57056a91d9070
Nedko Nedev <38923501+NedkoPNedev@users.noreply.github.com>
2019-07-27 17:39:05 +0300

    Add files via upload

---
+++ chapter02/src/main/java/tacos/HomeController.java
@@ -0,0 +1,21 @@
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping
+	public String home() {
+		return "home";
+	}
+	
+	@PostMapping
+	public String processQuery() {
+		return "design";
+	}
+}
